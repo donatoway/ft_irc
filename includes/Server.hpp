739c6153508bc9170b444265e88dbcef2b357bd0
donatoway <donatoway@gmail.com>
2022-10-26 17:27:42 +0200

    m

--- includes/Server.hpp
+++ includes/Server.hpp
@@ -4,6 +4,10 @@
 #pragma once
 
 #include <unistd.h>
+#include <string>
+#include <string.h>
+#include <stdlib.h>
+#include <iostream>
 #include <fcntl.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
@@ -22,13 +26,26 @@
 
 #define MAX_CONNECTIONS 200
 
+#define	connect			500
+#define	disconnect		501
+#define join			502
+#define part			503
+#define set_nick		504
+#define nick			505
+#define whois			506
+#define msg				507
+#define kick			508
+#define ban				509
+
 
 class Server
 {
 	private:
+		std::vector<std::string> cmds;
 		std::string				host;
 		std::string				port;
 		std::string				password;
+		std::string				buffer;
 		int						sock;
 		int						running;
 		std::vector<pollfd>		poll_fds;
@@ -44,6 +61,8 @@ class Server
 		std::string	recive(int fd);
 		int			handle_message(int fd);
 		void		handle_disconnection(int fd);
+		void		execute_cmd(int	cmd);
+		int			parse_cmd(std::string cmd);
 };
 
 #endif
--- srcs/Server.cpp
+++ srcs/Server.cpp
@@ -1,5 +1,5 @@
 #include "Server.hpp"
-
+#include "Client.hpp"
 Server::Server(std::string const &port, std::string const &password)
 {
 	this->host = "127.0.0.1";
@@ -18,7 +18,6 @@ Server::~Server()
 
 void	Server::start()
 {
-	//poll init
 	pollfd	server_fd = {this->sock, POLLIN | POLLHUP, 0};
 	poll_fds.push_back(server_fd);
 
@@ -89,7 +88,7 @@ void	Server::handle_connection()
 	int			fd;
 	sockaddr_in	addr = {};
 	socklen_t 	size;
-	char		msg[1000];
+	char		_msg[1000];
 
 	// accept connection
 	size = sizeof(addr);
@@ -105,13 +104,13 @@ void	Server::handle_connection()
 	Client *new_client = new Client(fd, inet_ntoa(addr.sin_addr), ntohs(addr.sin_port));
 	this->clients.insert(std::make_pair(fd, new_client));
 	// log new connection
-	sprintf(msg, "%s:%d has connected", new_client->getHostname().c_str(), new_client->getPort());
-	console_log(msg);
+	sprintf(_msg, "%s:%d has connected", new_client->getHostname().c_str(), new_client->getPort());
+	console_log(_msg);
 }
 
 std::string	Server::recive(int fd)
 {
-	std::string	msg;
+	std::string	_msg;
 	char		buffer[100];
 
 	bzero(buffer, 100);
@@ -127,24 +126,25 @@ std::string	Server::recive(int fd)
 		// clear buffer means exit (UNIX)
 		if (!buffer[0])
 			return ("");
-		msg.append(buffer);
+		_msg.append(buffer);
 	}
-	return (msg);
+	return (_msg);
 }
 
 int	Server::handle_message(int fd)
 {
-	std::string msg = this->recive(fd);
+	std::string _msg = this->recive(fd);
 	// if disconnected
-	if (msg[0] == 0)
+	if (_msg[0] == 0)
 	{
 		this->handle_disconnection(fd);
 		return (1);
 	}
 	// TODO command handler
-	console_log("TODO command handler");
+	//console_log("TODO command handler");
+	execute_cmd(parse_cmd(_msg));
 	// TODO command handler
-	console_log(msg);
+	console_log(_msg);
 	return (0);
 }
 
@@ -153,11 +153,11 @@ void	Server::handle_disconnection(int fd)
 	try
 	{
 		Client	*client = this->clients.at(fd);
-		char 	msg[1000];
+		char 	_msg[1000];
 
 		// message of disconnection
-		sprintf(msg, "%s:%d has disconnected.", client->getHostname().c_str(), client->getPort());
-		console_log(msg);
+		sprintf(_msg, "%s:%d has disconnected.", client->getHostname().c_str(), client->getPort());
+		console_log(_msg);
 		// remove the client
 		this->clients.erase(fd);
 		for (std::vector<pollfd>::iterator it = poll_fds.begin(); it != poll_fds.end(); ++it)
@@ -172,4 +172,45 @@ void	Server::handle_disconnection(int fd)
 	}
 	catch (std::out_of_range const &err) {}
 }
+
+int		Server::parse_cmd(std::string cmd)
+{
+
+
+	//creare una funzione che splitta comando e argomento
+	// e li salva in un vettore
+	if (cmd.compare("nick\n") == 0) return (nick);
+	if (cmd.compare("connect\n") == 0) return (connect);
+	if (cmd.compare("disconnect\n") == 0) return (disconnect);
+	if (cmd.compare("join\n") == 0) return (join);
+	if (cmd.compare("part\n") == 0) return (part);
+	if (cmd.compare("set nick\n") == 0) return (set_nick);
+	if (cmd.compare("whois\n") == 0) return (whois);
+	if (cmd.compare("msg\n") == 0) return (msg);
+	if (cmd.compare("kick\n") == 0) return (kick);
+	if (cmd.compare("ban\n") == 0) return (ban);
+	return (0);
+}
+
+void	Server::execute_cmd(int	cmd)
+{
+	switch (cmd)
+	{
+	case nick : console_log("CMD: comando scelto nick\n"); break;
+	case connect : console_log("CMD: comando scelto connect\n");break;
+	case disconnect : console_log("CMD: comando scelto disconnect\n");break;
+	case join : console_log("CMD: comando scelto join\n");break;
+	case part : console_log("CMD: comando scelto part\n");break;
+	case set_nick : console_log("CMD: comando scelto set nick\n");break;
+	case whois : console_log("CMD: comando scelto  whois\n");break;
+	case msg : console_log("CMD: comando scelto  msg\n");break;
+	case kick : console_log("CMD: comando scelto set kick\n");break;
+	case ban : console_log("CMD: comando scelto  ban\n");break;
+
+	default: console_log("CMD: Command not found\n");
+	break;
+	}
+}
+
+
 
